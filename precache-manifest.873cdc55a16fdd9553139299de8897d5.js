5af942d4115128cd1c4b10da90cec6c1a97453d8
MirRizwan <mir_rizwan@yahoo.com>
2020-10-11 02:40:05 +0400

    Updates

---
+++ precache-manifest.873cdc55a16fdd9553139299de8897d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab35d66465ae418a5f4ea8b0d78e4986",
+    "url": "/rizwanclass/index.html"
+  },
+  {
+    "revision": "973f537c9b5469fb7ae6",
+    "url": "/rizwanclass/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b6456501937555fa6dc2",
+    "url": "/rizwanclass/static/js/2.8c230bfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rizwanclass/static/js/2.8c230bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973f537c9b5469fb7ae6",
+    "url": "/rizwanclass/static/js/main.f94f2408.chunk.js"
+  },
+  {
+    "revision": "94a888a2cab8c108eef5",
+    "url": "/rizwanclass/static/js/runtime-main.cd6db127.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rizwanclass/static/media/logo.5d5d9eef.svg"
+  }
+]);
